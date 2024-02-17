5d91c1ab584dc7801fb81318ca785d95429f43e7
debg48 <debgandhar4000@gmail.com>
2024-02-17 20:18:42 +0530

    modified to basic logging server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
+// creating a simple logger
+
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
-    console.log("New Request Received");
-    res.end("Hello From Server");
+    const log = `${Date.now()}: New Request Received\n`;
+    fs.appendFile("log.txt",log,(err,data)=>
+    {
+        res.end("Hello From Server");
+    });    
 })
 
 myServer.listen(8000,() => console.log("Server Started"));
