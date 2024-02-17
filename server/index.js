3351710669f0953e4c838d97f0c77f5c08d778cf
debg48 <debgandhar4000@gmail.com>
2024-02-17 19:59:40 +0530

    basic http server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-const http = requires("http");
+const http = require("http");
 
-const myServer = http.createServer((req,res)=>){
-    console.log("New Request Received")
-}
+const myServer = http.createServer((req,res)=>{
+    console.log("New Request Received");
+    res.end("Hello From Server");
+})
+
+myServer.listen(8000,() => console.log("Server Started"));
