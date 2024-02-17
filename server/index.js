eae8d4a133286f1431c73bf612d790ce23c5b9e2
debg48 <debgandhar4000@gmail.com>
2024-02-17 20:30:06 +0530

    switch cases added

--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
-    const log = `${Date.now()}: New Request Received\n`;
+    const log = `${Date.now()}:${req.url} New Request Received\n`;
     fs.appendFile("log.txt",log,(err,data)=>
     {
-        res.end("Hello From Server");
+        switch(req.url){
+            case '/' : res.end("Home Page");
+            break
+            case '/about' : res.end("I am Tom Marvolo Riddle")
+            break
+            default : res.end("404");
+        }
     });    
 })
 
