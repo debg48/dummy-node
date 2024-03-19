24677973a6c3466046ac53c93cdde6f8c9348fdc
debg48 <debgandhar4000@gmail.com>
2024-03-19 17:24:29 +0530

    removed http module dependency

--- express/index.js
+++ express/index.js
@@ -1,4 +1,4 @@
-const http = require("http")
+// const http = require("http")
 const express = require("express")
 
 const app = express();  //app is basically a handler function
@@ -11,6 +11,8 @@ app.get('/about',(req,res) =>{
     return res.send("About Page")
 } );
 
-const myServer = http.createServer(app);
+app.listen(8000,() => console.log("Server Started"))
 
-myServer.listen(8000,() => console.log("Server Started"))
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000,() => console.log("Server Started"))
