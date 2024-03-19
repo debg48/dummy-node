e99933a31f04d513a9d0db2416d4386f94a5523c
debg48 <debgandhar4000@gmail.com>
2024-03-19 16:48:31 +0530

    express setup

---
+++ express/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express();  //app is basically a handler function
+
+app.get('/',(req,res) =>{
+    return res.send("Home Page")
+} ); //handler ,specify method and path , handler function for hte route
+
+app.get('/about',(req,res) =>{
+    return res.send("About Page")
+} );
