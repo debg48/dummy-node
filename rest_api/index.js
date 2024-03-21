2c790eacbf81990540f17a60c00becd9b499b5ff
debg48 <debgandhar4000@gmail.com>
2024-03-21 18:11:16 +0530

    server side rendering demo at /users

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
 const app = express();
-const PORT = 8000
+const PORT = 8000;
 
 //routes
-
 app.get("/users",(req,res)=>{
-    return res.json(users)
+    const html = `
+    <ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
 })
 
-app.listen(PORT,() => console.log(`Server Started at Port: ${PORT}`)) 
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+})
+
+app.listen(PORT,() => console.log(`Server Started at Port: ${PORT}`));
 
