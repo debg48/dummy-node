b9657fc65c60d4c2aa4b19af1d37cb3dfbbe8de1
debg48 <debgandhar4000@gmail.com>
2024-03-22 14:34:24 +0530

    post route adeed

--- rest_api/index.js
+++ rest_api/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
+const fs = require("fs");
 
 //middleware
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended:false }));
 
 
 //routes
@@ -15,33 +16,35 @@ app.get("/users",(req,res)=>{
     </ul>
     `;
     res.send(html);
-})
+});
 
 app.get("/api/users",(req,res)=>{
     return res.json(users);
-})
+});
 
 app.get("/api/users/:id",(req,res)=>{
     const id = Number(req.params.id); //string hence we convert to Number
     const user = users.find((user) => user.id === id);
-    return res.json(user)
-})
+    return res.json(user);
+});
 
 app.post("/api/users/",(req,res)=>{
-    //TODO
-    const body = req.body
-    return res.json({status : "pending"});
-})
+    const body = req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err, data)=>{
+        return res.json({status : "success" , id : users.length});
+    })
+});
 
 app.patch("/api/users/:id",(req,res)=>{
     //TODO
     return res.json({status : "pending"});
-})
+});
 
 app.delete("/api/users/:id",(req,res)=>{
     //TODO
     return res.json({status : "pending"});
-})
+});
 
 
 app.listen(PORT,() => console.log(`Server Started at Port: ${PORT}`));
