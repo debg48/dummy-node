c4d3e4254cee8891cf5f80e047229b96083396c7
debg48 <debgandhar4000@gmail.com>
2024-04-01 22:09:47 +0530

    all handler function added to controller{

--- mvc/controller/user.js
+++ mvc/controller/user.js
@@ -1,8 +1,53 @@
 const User = require("../model/user")
-async function handleGetAllUSer(req,res){
+
+async function handleGetAllUser(req,res){
     const allDbUser = await User.find({});
     return res.json(allDbUser);
 }
-module.export = {
-    handleGetAllUSer,
+
+async function handleGetUserbyId(req,res){
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({error : "User not found !"});
+    return res.json(user);
+}
+
+async function handleUpdateUserbyId(req,res){
+    //this is just for testing purpose
+    await User.findByIdAndUpdate(req.params.id,{lastName : "Changed"});
+    return res.json({status : "Success"})
+}
+
+async function handleDeleteUserbyId(req,res){
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({status : "Success"})
+}
+
+async function handleCreateNewUser(req,res){
+    const body = req.body;
+    if(
+        !body || 
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ){
+        return res.status(400).json({msg:"All fields reqd!"});
+    }
+    const result = await User.create({
+        firstName : body.first_name ,
+        lastName: body.last_name ,
+        emai : body.email ,
+        gender : body.gender ,
+        jobTitle: body.job_title
+    });
+    return res.status(201).json({msg:"success",id : result._id})
+}
+
+module.exports = {
+    handleGetAllUser,
+    handleGetUserbyId,
+    handleUpdateUserbyId,
+    handleDeleteUserbyId,
+    handleCreateNewUser,
 };
--- mvc/routes/user.js
+++ mvc/routes/user.js
@@ -1,32 +1,13 @@
 const express = require("express");
+
+const {handleGetAllUser,handleGetUserbyId,handleUpdateUserbyId,handleDeleteUserbyId,handleCreateNewUser} = require("../controller/user")
+
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    return res.json(users);
-});
-
-router.get("/:id",(req,res)=>{
-    const id = Number(req.params.id); //string hence we convert to Number
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
-
-router.post("/",(req,res)=>{
-    const body = req.body;
-    users.push({...body,id:users.length+1});
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err, data)=>{
-        return res.json({status : "success" , id : users.length});
-    })
-});
-
-router.patch("/:id",(req,res)=>{
-    //TODO
-    return res.json({status : "pending"});
-});
-
-router.delete("/:id",(req,res)=>{
-    //TODO
-    return res.json({status : "pending"});
-});
+router.get("/",handleGetAllUser);
+router.get("/:id",handleGetUserbyId);
+router.post("/",handleCreateNewUser);
+router.patch("/:id",handleUpdateUserbyId);
+router.delete("/:id",handleDeleteUserbyId);
 
 module.exports = router;
