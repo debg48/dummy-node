f333fa3925350463423311985e68b7bcca03a37c
debg48 <debgandhar4000@gmail.com>
2024-06-21 16:12:48 +0530

    added commands to bot

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -5,12 +5,25 @@ const client = new Client({
 require("dotenv").config();
 
 client.on('messageCreate', (message) => {
+    if (message.content.startsWith('create')) {
+        const url = message.content.split('create')[1];
+        return message.reply(
+            {
+                content: "Generating short ID for " + url, 
+            }
+        )
+    }
     if (message.author.bot) return;
     message.reply(
         {
             content: "Hello from NodeJS BOT"
         }
     )
+});
+
+client.on('interactionCreate', (interaction) => {
+    // console.log(interaction);
+    interaction.reply("Interaction Response!");
 });
 
 client.login(process.env.ACCESS_TOKEN);
