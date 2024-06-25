554ccaca306030d3cbc18cdd04d365cd06677a00
debg48 <debgandhar4000@gmail.com>
2024-06-25 11:41:26 +0530

    file upload feature working

--- file_upload/index.js
+++ file_upload/index.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer')
+
+// const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 const port = 8000;
 
-// Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
-// Middleware to parse JSON
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Route for homepage
+
 app.get('/', (req, res) => {
     res.render('homepage');
 });
 
-// Start the server
+app.post('/upload', upload.single('profileImage'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect("/");
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
