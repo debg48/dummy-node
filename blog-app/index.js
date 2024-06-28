a652b1ca1e328cc3317a55c9797d930e242bbd82
debg48 <debgandhar4000@gmail.com>
2024-06-28 12:54:04 +0530

    navbar added

--- blog-app/index.js
+++ blog-app/index.js
@@ -7,4 +7,8 @@ const PORT = 8000;
 app.set('view engine', 'ejs');
 app.set('views',path.resolve('./views'));
 
+app.get('/', (req,res)=>{
+    return res.render('home');
+});
+
 app.listen(PORT,() => console.log(`Server started at PORT: ${PORT}`));
