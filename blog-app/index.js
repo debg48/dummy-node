e589a569b43f4f64fe4d0903c101c708ada9d30d
debg48 <debgandhar4000@gmail.com>
2024-08-21 12:05:34 +0530

    added authentication and authorization

--- blog-app/index.js
+++ blog-app/index.js
@@ -1,14 +1,32 @@
 const path = require("path");
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const { checkForAuthenticationCookie } = require("./middlewares/auth");
 
 const app = express();
 const PORT = 8000;
 
+mongoose.connect('mongodb://localhost:27017/blogify').then(e => console.log('Mongo DB connected!'));
+
 app.set('view engine', 'ejs');
 app.set('views',path.resolve('./views'));
 
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
+
 app.get('/', (req,res)=>{
-    return res.render('home');
+    return res.render('home',{
+        user : req.user,
+    });
 });
+
+app.use('/user',userRoute);
+app.use('/blog',blogRoute);
 
 app.listen(PORT,() => console.log(`Server started at PORT: ${PORT}`));
