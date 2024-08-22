3b5693533b142e548756e48b5cab5fc8e4984d30
debg48 <debgandhar4000@gmail.com>
2024-08-22 12:00:53 +0530

    added render all blogs on homepage

--- blog-app/index.js
+++ blog-app/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
+const Blog = require('./models/blog');
 
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
@@ -19,10 +20,13 @@ app.set('views',path.resolve('./views'));
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve("./public")));
 
-app.get('/', (req,res)=>{
+app.get('/', async (req,res)=>{
+    const allBlogs = await Blog.find({});
     return res.render('home',{
         user : req.user,
+        blogs: allBlogs,
     });
 });
 
